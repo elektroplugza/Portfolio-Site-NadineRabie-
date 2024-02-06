67cc45787e48bda60bb13a16131614f91a9ef847
nadinerabie <nadinerabie8@gmail.com>
2024-02-06 16:59:55 +0200

    Created/navtoggle-event-listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
-console.log("Hi, welcome to my portfolio site!")
+// Add an event listener to the document
+document.addEventListener('click', function(event) {
+    var menu = document.getElementById('myNavtoggle');
+    var target = event.target;
+
+    // Check if the click target is not inside the menu
+    if (!menu.contains(target)) {
+        // Close the menu if it's open
+        if (menu.classList.contains('responsive')) {
+            menu.classList.remove('responsive');
+        }
+    }
+});
+
+// Function to toggle the menu
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.classList.contains('responsive')) {
+        x.classList.remove('responsive');
+    } else {
+        x.classList.add('responsive');
+    }
+}
+
+
+
+
+/* function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  } */
